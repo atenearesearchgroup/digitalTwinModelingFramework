9ecb8404967f3fa21c67e8a3ed4032f0d510ae8a
Paula <paulam@uma.es>
2022-03-18 12:48:00 +0100

    [Traces visualization] Trace analysis in Python updated.

--- tracesAnalysis/src/traces_retriever.py
+++ tracesAnalysis/src/traces_retriever.py
@@ -1,8 +1,24 @@
+import os
+
 import redis
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
-execution_id = "1646999743"
+execution_id = "1647443878"
+path = './output_data/'
+filename_csv = path + execution_id + '.csv'
+filename_3d_graph = path + execution_id + 'Fig3D.png'
+filename_2d_graph = path + execution_id + 'Fig2D.png'
+
+if os.path.exists(filename_csv):
+    os.remove(filename_csv)
+    os.remove(filename_3d_graph)
+    os.remove(filename_2d_graph)
+    print("[INFO] The files have been deleted successfully")
+
+f = open(filename_csv, 'w', newline='')
+writer = csv.writer(f, delimiter=";")
 
 r = redis.Redis(host="localhost", port=6379)
 snapshots = r.zrange("PT:NXJCar:" + execution_id + ":EXECUTIONID_LIST", 0, -1, False)
@@ -16,7 +32,10 @@ for snp in snapshots:
     values = r.hgetall(snp)
     x_pos = float(values['xPos'.encode('utf-8')])
     y_pos = float(values['yPos'.encode('utf-8')])
-    time = int(values['timestamp'.encode('utf-8')]) - int(execution_id)
+    timestamp = int(values['timestamp'.encode('utf-8')])
+    time = timestamp - int(execution_id)
+    action = str(values['action'.encode('utf-8')]).replace('b\'', '').replace('\'', '')
+    writer.writerow([x_pos, y_pos])
     ax.scatter(x_pos, y_pos, time, marker='^')
     ax2.scatter(x_pos, y_pos, marker='o')
 
@@ -27,4 +46,11 @@ ax.set_zlabel('Time')
 ax2.set_xlabel('xPos')
 ax2.set_ylabel('yPos')
 
+fig.savefig(filename_3d_graph)
+fig2.savefig(filename_2d_graph)
+
 plt.show()
+plt.close(fig)
+plt.close(fig2)
+
+f.close()
