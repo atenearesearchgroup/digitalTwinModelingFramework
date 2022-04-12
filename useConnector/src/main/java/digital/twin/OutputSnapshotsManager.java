5f45e72436ea5bff9a64b77aacec05cf76860ac8
Paula <paulam@uma.es>
2022-04-12 12:37:38 +0200

    [Framework generalization - USE Plugin] Added configuration file to specify the characteristics of the Digital Twin.

--- useConnector/src/main/java/digital/twin/OutputSnapshotsManager.java
+++ useConnector/src/main/java/digital/twin/OutputSnapshotsManager.java
@@ -5,8 +5,12 @@ import org.tzi.use.api.UseSystemApi;
 import org.tzi.use.uml.mm.MAttribute;
 import org.tzi.use.uml.ocl.value.Value;
 import org.tzi.use.uml.sys.MObjectState;
+import org.yaml.snakeyaml.Yaml;
 import redis.clients.jedis.Jedis;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,41 +20,35 @@ import java.util.Map;
  */
 public class OutputSnapshotsManager extends OutputManager {
 
+    final String CONFIGURATION_FILE_PATH = "./DTConfig.yaml";
+    final String RETRIEVED_CLASS = "retrievedClass";
+    final String ATTRIBUTES = "attributes";
+
     /**
      * It sets the type of the attributes in a HashMap to parse the attributes for the Data Lake.
      * For example, Booleans will turn into 0 or 1; Numbers will be transformed into Floats.
-     * TODO: Support a configuration file that determines the format of the information to be received, specifying which attributes we want to receive and store.
      */
+    @SuppressWarnings("unchecked")
     public OutputSnapshotsManager() {
         super();
         this.setChannel("DTOutChannel");
-        this.retrievedClass = "OutputCarSnapshot";
+        Map<String, Object> configurationParameters = loadConfigurationFile();
+
+        this.retrievedClass = configurationParameters.get(RETRIEVED_CLASS).toString();
         this.identifier = "processedSnapsDT";
 
-        attributes.put("twinId", STRING);
-        attributes.put("timestamp", NUMBER);
-        attributes.put("executionId", NUMBER);
-
-        attributes.put("xPos", NUMBER);
-        attributes.put("yPos", NUMBER);
-        attributes.put("angle", NUMBER);
-        attributes.put("speed", NUMBER);
-
-        attributes.put("light", NUMBER);
-        attributes.put("distance", NUMBER);
-        attributes.put("bump", BOOLEAN);
-        attributes.put("isMoving", BOOLEAN);
-
-        attributes.put("action", STRING);
+        Map<String, Object> attsConfiguration = (Map<String, Object>) configurationParameters.get(ATTRIBUTES);
+        for(String attName : attsConfiguration.keySet()){
+            attributes.put(attName, attsConfiguration.get(attName).toString());
+        }
     }
 
     /**
      * It saves all the OutputCarSnapshots object in the currently displayed object diagram in the data lake.
      * Then, it removes them from the diagram.
      *
-     * @param api   USE system API instance to interact with the currently displayed object diagram.
+     * @param api   USE system API instances to interact with the currently displayed object diagram.
      * @param jedis An instance of the Jedis client to access the data lake.
-     * @throws UseApiException
      */
     public void saveObjects(UseSystemApi api, Jedis jedis) throws UseApiException {
         List<MObjectState> outputSnapshots = this.getObjects(api);
@@ -63,4 +61,14 @@ public class OutputSnapshotsManager extends OutputManager {
         }
     }
 
+    private Map<String, Object> loadConfigurationFile() {
+        InputStream inputStream = null;
+        Yaml yaml = new Yaml();
+        try {
+            inputStream = new FileInputStream(CONFIGURATION_FILE_PATH);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return yaml.load(inputStream);
+    }
 }
